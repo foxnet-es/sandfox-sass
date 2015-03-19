979b5dfab105e3f6143b52a1aa1e87f299b79a9e
ruben.lopez <loslopez1@icloud.com>
2015-03-19 02:23:05 +0100

    add: gruntfile configuration with node-sass opts

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function( grunt ) {
+
+    var path        = require( "path" ),
+
+        pkg         = grunt.file.readJSON(  "package.json" ),
+
+        config      = 
+        ({
+        
+            pkg         : pkg,
+
+            sass:
+            {
+                options:
+                {
+                    style: 'expanded',
+
+                    sourceMap: true
+                },
+
+                dev:
+                {
+                    files:
+                    {
+                        './dist/sandfox.css'     : './src/main.scss'
+                    }
+                }
+            },
+
+            watch       : 
+            {
+                css     :
+                {
+                    files       : [ './package.json', './src/*.scss', './src/**/*.scss' ],
+
+                    tasks       : [ 'sass' ]
+                }
+            },
+        });
+
+    grunt.initConfig( config );   
+
+    grunt.loadNpmTasks( 'grunt-sass' );
+
+    grunt.loadNpmTasks( 'grunt-contrib-watch' );
+
+    grunt.registerTask( 'dev',      [ 'sass:dev' ] );
+
+    grunt.registerTask( 'master',   [ 'sass:master' ] );
+
+    grunt.registerTask( 'test',     [ 'karma:unit' ] );
+
+    grunt.registerTask( 'default',  [ 'dev' ] );
+};
